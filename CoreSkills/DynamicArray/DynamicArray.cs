0f7103922fc7ac6c9b627fe3a582441713d058e4
altair961 <altair89@list.ru>
2024-08-21 07:39:52 +0300

    A Solution for DynamicArray problem was added.

---
+++ CoreSkills/DynamicArray/DynamicArray.cs
@@ -0,0 +1,73 @@
+﻿namespace DynamicArray
+{
+    public class DynamicArray
+    {
+        private int[] _array;
+        private int _capacity;
+        private int _elementsNumber;
+
+        public DynamicArray(int capacity)
+        {
+            _array = new int[capacity];
+            _capacity = capacity;
+            _elementsNumber = 0;
+        }
+
+        public int Get(int i)
+        {
+            var result = _array[i];
+            return result;
+        }
+
+        public void Set(int i, int n)
+        {
+            _array[i] = n;
+        }
+
+        public void PushBack(int n)
+        {
+            if (_elementsNumber == _capacity)
+            {
+                Resize();
+            }
+            _array[_elementsNumber] = n;
+            _elementsNumber++;
+        }
+
+        public int PopBack()
+        {
+            _elementsNumber--;
+            var lastValue = _array[_elementsNumber];
+            var shortenedArr = new int[_elementsNumber];
+
+            for (int i = 0; i < _elementsNumber; i++)
+            {
+                shortenedArr[i] = _array[i];
+            }
+
+            _array = shortenedArr;
+
+            return lastValue;
+        }
+
+        private void Resize()
+        {
+            _capacity *= 2;
+            var extendedArr = new int[_capacity];
+            for (int i = 0; i < _elementsNumber; i++)
+                extendedArr[i] = _array[i];
+
+            _array = extendedArr;
+        }
+
+        public int GetSize()
+        {
+            return _elementsNumber;
+        }
+
+        public int GetCapacity()
+        {
+            return _capacity;
+        }
+    }
+}
---
+++ CoreSkills/DynamicArray/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DynamicArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ["Array", 4, "pushback", 1, "set", 0, 0, "pushback", 2, "get", 0, "get", 1,
+            // "getCapacity", "popback"]
+            // your    [null,null,null,null,0,2,2,2]
+            // correct [null,null,null,null,0,2,4,2]
+
+            var dynamicArr = new DynamicArray(4);       // null
+            dynamicArr.PushBack(1);                     // null
+            dynamicArr.Set(0, 0);                       // null
+            dynamicArr.PushBack(2);                     // null
+            var getRes0 = dynamicArr.Get(0);            // 0
+            var getRes1 = dynamicArr.Get(1);            // 2 
+            var capacity1 = dynamicArr.GetCapacity();   // 2 sh be 4
+            var popBackRes1 = dynamicArr.PopBack();     // 2
+
+            /*
+            // ["Array", 1, "pushback", 1, "getCapacity", "pushback", 2, "getCapacity"]
+            // your    [null,null,1,null,1]
+            // correct [null,null,1,null,2]
+
+            var dynamicArr = new DynamicArray(1);       // null
+            dynamicArr.PushBack(1);                     // null
+            var capacity1 = dynamicArr.GetCapacity();   // 1
+            dynamicArr.PushBack(2);                     // null
+            var capacity2 = dynamicArr.GetCapacity();   // 2
+             */
+        }
+    }
+}
