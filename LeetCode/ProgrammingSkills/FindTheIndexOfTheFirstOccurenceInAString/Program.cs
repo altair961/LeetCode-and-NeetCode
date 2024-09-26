fde4f1f0b859270e8b991cc5443a93590f55be02
altair961 <altair89@list.ru>
2024-09-26 06:47:35 +0300

    Solution for FindTheIndexOfTheFirstOccurenceInAString was implemented

---
+++ LeetCode/ProgrammingSkills/FindTheIndexOfTheFirstOccurenceInAString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FindTheIndexOfTheFirstOccurenceInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            //var haystack = "sadbutsad";
+            //var needle = "sad";
+
+            //var haystack = "satdbutsadsad";
+            //var needle = "sad";
+
+            //var haystack = "leetcode";
+            //var needle = "leeto";
+
+            //var haystack = "hello";
+            //var needle = "ll";
+
+            //var haystack = "mississippi";
+            //var needle = "pi";
+
+            var haystack = "babbbbbabb";
+            var needle = "bbab";
+
+            var result = solution.StrStr(haystack, needle);
+        }
+    }
+}
---
+++ LeetCode/ProgrammingSkills/FindTheIndexOfTheFirstOccurenceInAString/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace FindTheIndexOfTheFirstOccurenceInAString
+{
+    internal class Solution
+    {
+        public int StrStr(string haystack, string needle) 
+        {
+            var haystackCurrentPieceSum = 0;
+            var needleSum = 0;
+            for (var j = 0; j < needle.Length; j++)
+                needleSum += needle[j];
+
+            for (var i = 0; i < haystack.Length; i++)
+            {
+                haystackCurrentPieceSum += haystack[i];
+
+                if (i >= needle.Length)
+                {
+                    haystackCurrentPieceSum -= haystack[i - needle.Length];
+                }
+
+                if (haystackCurrentPieceSum == needleSum)
+                {
+                    var haystackCurrentPieceBeginningIndex = i - needle.Length + 1;
+                    var needleIndex = 0;
+                    for (int k = haystackCurrentPieceBeginningIndex; needleIndex < needle.Length; k++, needleIndex++)
+                    {
+                        if (haystack[k] != needle[needleIndex])
+                        {
+                            break;
+                        }
+
+                        if (needleIndex == needle.Length - 1)
+                        {
+                            return k - (needle.Length - 1); 
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
